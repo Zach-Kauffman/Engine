3611e3c8a9d5bdf758e20596a303d6d6e1c91d91
Nathan Jewell <nathanielx.jewell@gmail.com>
2015-12-10 21:03:32 -0800

    Added readWriteMap() function and cleaned up filesystem/main.cpp Implemented a function allowing maps to be read with INIReader. Removed unecessary files from a weird extra project directory. Cleaned up main.cpp a bit by removing extra functions and replaces vector test case with map test case. Fixes #4.

--- Engine/Engine/Main.cpp
+++ Engine/Engine/Main.cpp
@@ -1,50 +1,34 @@
-#include <cstddef>
-#include <string>
-#include <ostream>
-#include <fstream>
-#include <iomanip>
-#include <boost/shared_ptr.hpp>
-#include <boost/make_shared.hpp>
-#include <boost/date_time/posix_time/posix_time.hpp>
-#include <boost/log/core.hpp>
-#include <boost/log/expressions.hpp>
-#include <boost/log/attributes.hpp>
-#include <boost/log/sources/basic_logger.hpp>
-#include <boost/log/sources/severity_logger.hpp>
-#include <boost/log/sources/record_ostream.hpp>
-#include <boost/log/sinks/sync_frontend.hpp>
-#include <boost/log/sinks/text_ostream_backend.hpp>
-#include <boost/log/attributes/scoped_attribute.hpp>
-#include <boost/log/utility/setup/common_attributes.hpp>
 #include "Logger.h"
 #include "INIReader.h"
 
 
 void logging_function()
 {
-	//BOOST_LOG_NAMED_SCOPE("INI_Reader_Testing");
+	//BOOST_LOG_NAMED_SCOPE("INI_Reader_Testing");											<<--- adds specified namespace to logs in all nested scopes
+	//BOOST_LOG_SCOPED_THREAD_ATTR("Timeline", attrs::timer()));							<<--- adds timer to logs in all nested scopes
+	//severity_logger.add_attribute("Tag", attrs::constant<std::string>("<yourstring>"));	<<--- adds tag attribute in brackets ex. [<yourstring>]
+
 	INIReader reader("config.ini");
 	
-	std::vector<std::string> variables;
-	variables.push_back("Beans.Ehhh");
-	variables.push_back("Beans.Meh");
-	variables.push_back("Beans.Heh");
-	std::vector<int> output = reader.readVector<int>(variables);
+	int one, two, three;
+	std::map<std::string, int*> variables;
+	variables["Beans.Ehhh"] = &one;
+	variables["Beans.Meh"] = &two;
+	variables["Beans.Heh"] = &three;
+
+	
+	
+	reader.readWriteMap<int>(variables);
 	auto slg = logger::getSLogger();
-	slg.add_attribute("Scope", attrs::constant<std::string>("INITESTING"));
+	slg.add_attribute("Scope", attrs::named_scope());
 	BOOST_LOG_SEV(slg, DEBUG) << "This is the first value as a string. " << reader.readValue<std::string>("Beans.Smoky");
-	BOOST_LOG_SEV(slg, ERROR) << "Array values as int. " << output[0] << " + " << output[1];
-	BOOST_LOG_SEV(slg, WARNING) << "Everything crumbles, shoot me now!";
-	BOOST_LOG_SEV(slg, INFO) << "2 + 2 = 4!";
-	BOOST_LOG_SEV(slg, FATAL) << "Insert fatal error here";
+	BOOST_LOG_SEV(slg, ERROR) << "Array values as int. " << one << " + " << two;
 
 	BOOST_LOG_SCOPED_THREAD_ATTR("Timeline", attrs::timer());
 
 	BOOST_LOG_SEV(slg, INFO) << "Starting to time nested functions";
 
 	BOOST_LOG_SEV(slg, INFO) << "Stopping to time nested functions";
-
-	slg.add_attribute("Tag", attrs::constant< std::string >("My tag value"));
 
 	BOOST_LOG_SEV(slg, INFO) << "Here goes the tagged record";
 }
@@ -75,7 +59,8 @@ int main(int, char*[])
 	logger::init();
 	logger::setSeverityLevel(DEBUG);
 	logging_function();
-
+	auto slg = logger::getSLogger();
+	BOOST_LOG_SEV(slg, DEBUG) << "Exiting soon";
 	//while (true){}
 	return 0;
 }
