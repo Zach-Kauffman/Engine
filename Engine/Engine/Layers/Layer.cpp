582fb67b18b2fe024158da83250152cf3df10b7e
Arccosecantism <riley.d.waugh@gmail.com>
2016-01-07 08:39:22 -0800

    added scrollBounds to layers, NOT TO LAYERMANAGER

--- Engine/Engine/Layers/Layer.cpp
+++ Engine/Engine/Layers/Layer.cpp
@@ -9,8 +9,8 @@
 
 Layer::Layer()
 {
+	scrollBounded = false;				//by default, scrollBoundedness is false
 }
-
 
 Layer::~Layer()
 {
@@ -25,4 +25,36 @@ void Layer::setScrollSpeed(const sf::Vector2f& fspeed)
 sf::Vector2f Layer::getScrollSpeed()
 {
 	return scrollSpeed;
+}
+
+
+
+void setScrollBound(const double& nsBound, const unsigned int& index)
+{
+	scrollBounds[index] = nsBound;
+}
+
+void setAllScrollBounds(const double(&nsBound)[4])	//might not be correct syntax
+{
+	for (unsigned int i = 0; i < 4; i++)
+	{
+		scrollBounds[i] = nsBound[i];
+	}
+}
+
+double getScrollBound(const unsigned int& index)
+{
+	return scrollBounds[index];
+}
+
+
+
+void setScrollBoundedness(const bool& nscrollBounded)
+{
+	scrollBounded = nscrollBounded;
+}
+
+bool getScrollBoundedness()
+{
+	return scrollBounded;
 }
--- Engine/Engine/Layers/Layer.hpp
+++ Engine/Engine/Layers/Layer.hpp
@@ -14,10 +14,31 @@ public:
 
 	sf::Vector2f getScrollSpeed();						//get scroll speed
 
+	void setScrollBound(const double& nsBound, const unsigned int& index);
+														//sets a specific scrollBound
+
+
+	void setAllScrollBounds(const double(&nsBound)[4]);	//set all scrollBounds
+
+	double getScrollBound(const unsigned int& index);	//gets a scroll bound by index
+
+
+	void setScrollBoundedness(const bool& nscrollBounded);
+
+	bool getScrollBoundedness();
+
+
 private:
 
 	sf::Vector2f scrollSpeed;							//the fraction of the "normal" scroll speed at which a layer will scroll; a layer with
 														//.5 scroll speed will scroll half as quickly as a layer with 1 scroll speed; a layer with 0
 														//scroll speed will be stationary.
+
+	double[4] scrollBounds;								//does not work yet,
+														//the purpose is that a layer should not be able to scroll forever. see http://imgur.com/SzH03Ju for a lack of scroll bounds.
+
+	bool scrollBounded;									//true if the layer has active scrolling bounds
+
+	//enum boundPositions { Top = 0, Left, Bottom, Right };
 };
 
